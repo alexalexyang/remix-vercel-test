c688fa64ce9372146d08c54028688b6f9488083b
alexalexyang <alexalexyang@gmail.com>
2022-06-26 16:35:47 +0200

    Add format for time string

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import styled from "styled-components";
 
 const H1 = styled.h1`
@@ -10,31 +11,8 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <H1>Welcome to Remix</H1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+
+      <h2>{format(new Date(), "kk:mm")}</h2>
     </div>
   );
 }
