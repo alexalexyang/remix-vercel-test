b54921e3d48b95009940a37b0356b7a34142187a
alexalexyang <alexalexyang@gmail.com>
2022-06-26 17:06:33 +0200

    Get yr mth dd with native JS methods

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -9,15 +9,20 @@ const H1 = styled.h1`
 
 export default function Index() {
   const now = new Date();
-  const hr = now.getHours();
-  const min = now.getMinutes();
+  // const hr = now.getHours();
+  // const min = now.getMinutes();
+
+  const yr = now.getFullYear();
+  const mth = now.getMonth();
+  const dd = now.getDate();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <H1>Welcome to Remix</H1>
 
       <h2>
-        {hr}:{min}
+        {/* {hr}:{min} */}
+        {`${yr}-${mth}-${dd}`}
       </h2>
     </div>
   );
